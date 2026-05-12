0a234f86716b022b6732a19381180842da56d208
Thomas Beltran <thomas.santiago.beltran15@gmail.com>
2026-05-12 11:28:26 -0500

    Refactor CRUDFactura class to follow naming conventions

--- CRUD/CRUDFactura.py
+++ CRUD/CRUDFactura.py
@@ -2,17 +2,17 @@ from datetime import date
 from modelo.Factura import Factura
 from modelo.Cliente import Cliente
 
-class CRUDFactura:
+
+class CrudFactura:
     def __init__(self):
         # Lista interna de facturas registradas
         self._facturas = []
 
-    def CrearFactura(self, cliente: Cliente):
-        
+    def crear_factura(self, cliente: Cliente):
         factura = Factura(date.today(), cliente)
         self._facturas.append(factura)
-        cliente.AgregarFactura(factura)  # Relación cliente-factura
+        cliente.agregar_factura(factura)  # Relación cliente-factura
         return factura
 
-    def ListarFacturas(self):
-        return [str(facturas) for facturas in self._facturas]
+    def listar_facturas(self):
+        return [str(factura) for factura in self._facturas]
